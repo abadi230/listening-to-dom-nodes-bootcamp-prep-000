6f7bfb8d0f9608ef41a64c7c3fe031e6576ffa7d
Abdullah Bajaman <ambajaman@gmail.com>
2019-06-16 23:06:36 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
 
-/*******************
+/*******************Demonstrate Listening to Events on a DOM Node with addEventListener()********/
 const main = document.getElementById('main');
  
 main.addEventListener('click', function(event) {
   alert('I was clicked!');
 });
+
 
 const input = document.querySelector('input');
  
